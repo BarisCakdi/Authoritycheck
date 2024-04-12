f601735dcf389c3a0f5f3483a0e9fbfb75c346a1
Baris <112624513+BarisCakdi@users.noreply.github.com>
2024-04-12 23:10:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Authoritycheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba yetki kontrol protokolüne hoş geldiniz!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.Write("Lütfen kullanıcı adı oluşturunuz: ");
+            string kadi = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Lütfen şifre oluşturunuz: ");
+            int sifre = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Kullanıcı adı giriniz: ");
+            string x = Console.ReadLine();
+            string mesaj = $"Hoş geldin {kadi},";
+            string giris = $" sistem güvenli bir şekilde çalışmaktadır!";
+            if (x == kadi)
+            {
+                Console.Write("şifre giriniz: ");
+                int y = int.Parse(Console.ReadLine());
+                Console.Clear() ;
+                    if (y == sifre)
+                {
+                    mesaj = mesaj + giris;
+                }
+                    else
+                {
+                   mesaj = "Hata! lütfen tekrar deneyiniz!!!";
+                }
+
+            }
+            else
+            {
+                mesaj = "Hata! Lütfen tekrar deneyiniz!!!";
+            }
+            Console.WriteLine(mesaj);
+           
+        }
+    }
+}
